37b45ed47d108f96b5f59982a2c8c51a5c94b24e
Boris Korobeinikov <slon2k@gmail.com>
2019-08-31 19:30:55 +0600

    app-header

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,44 +7,31 @@ import GroupBlock from "../components/group-block";
 const HomePage = () => {
     return (
         <Container style={{"background: background": "background: #E5E5E5"}}>
-            <Title isSize={1}>isSize={1}</Title>
-            <Title isSize={2}>isSize={2}</Title>
-            <Title isSize={3}>isSize={3}</Title>
-            <Title isSize={4}>isSize={4}</Title>
-            <Title isSize={5}>isSize={5}</Title>
-            <Title isSize={6}>isSize={6}</Title>
-            <h3>HomePage</h3>
-            <Content>
-                <h1>Content</h1>
-                <p>Is the component to handle WYSIWYG generated content</p>
-            </Content>
-            {/*<Columns>*/}
-            {/*    <Column isSize={''}/>*/}
-            {/*</Columns>*/}
+            <Section>
+                <p style={{
+                    "font-family": "Roboto",
+                    "font-style": "normal",
+                    "font-weight": "bold",
+                    "font-size": "50px",
+                    "line-height": "64px",
+                    /* identical to box height, or 128% */
 
-            <p style={{
-                "font-family": "Roboto",
-                "font-style": "normal",
-                "font-weight": "bold",
-                "font-size": "50px",
-                "line-height": "64px",
-                /* identical to box height, or 128% */
+                    "text-align": "center",
+                    "letter-spacing": "0.892857px",
 
-                "text-align": "center",
-                "letter-spacing": "0.892857px",
-
-                "color": "#292A34"
-            }}>
-                Найдите подходящую группу
-            </p>
-            <GroupBlocks groups={[
-                <GroupBlock/>,
-                <GroupBlock/>,
-                <GroupBlock/>,
-                <GroupBlock/>,
-                <GroupBlock/>,
-                <GroupBlock/>]
-            }/>
+                    "color": "#292A34"
+                }}>
+                    Найдите подходящую группу
+                </p>
+                <GroupBlocks groups={[
+                    <GroupBlock/>,
+                    <GroupBlock/>,
+                    <GroupBlock/>,
+                    <GroupBlock/>,
+                    <GroupBlock/>,
+                    <GroupBlock/>]
+                }/>
+            </Section>
         </Container>
     );
 };
